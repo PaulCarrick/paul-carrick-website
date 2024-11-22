73208742c4fdb2b595b6f096f16ee46fe6f158b4
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-11-22 12:33:16 -0800

    Update contact_controller.rb

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,29 +6,28 @@ class ContactController < ApplicationController
   end
 
   def create
-    #    if verify_recaptcha(model: @contact) && @contact.save
-    if @contact.save
-      begin
-        mailer = ContactMailer.new
+    #    if verify_recaptcha(model: @contact)
+    begin
+      mailer = ContactMailer.new
 
-        mailer.send_email(params[:name], params[:email_address], params[:phone], params[:message])
+      mailer.contact_email(params[:name], params[:email_address], params[:phone], params[:message])
 
-        @notice = 'Conact form was successfully sent.'
-        flash[:info] = notice
-      rescue => e
-        @notice = "Could not send email. Error: #{e.message}."
-        flash[:error] = notice
-      end
-
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: @notice }
-      end
-
-      redirect_to root_path, notice: 'Your message has been sent successfully.'
-    else
-      flash.now[:alert] = 'There was an error with your submission. Please try again.'
-      render :new
+      @notice = "Contact form was successfully sent."
+      flash[:info] = notice
+    rescue => e
+      @notice = "Could not send email. Error: #{e.message}."
+      flash[:error] = notice
     end
+
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: @notice }
+    end
+
+    redirect_to root_path, notice: "Your message has been sent successfully."
+    #  else
+    #    flash.now[:alert] = 'There was an error with your submission. Please try again.'
+    #    render :new
+    # end
   end
 
   private
