cba0e5c4cac87e9fc5f0c198bc24b1d589de2f22
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-11-22 13:32:43 -0800

    Update contact_controller.rb

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -16,7 +16,7 @@ class ContactController < ApplicationController
       flash[:info] = notice
 
       respond_to do |format|
-        format.html { redirect_to root_url(target: "_self"), notice: @notice }
+        format.html { redirect_to controller: "home", action: "index", notice: @notice }
       end
     rescue => e
       @notice = "Could not send email. Error: #{e.message}."
