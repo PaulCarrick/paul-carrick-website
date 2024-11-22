9bc370639ecf57fdf8d6e3439f9048f4e841eeae
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-11-22 13:06:30 -0800

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,17 @@
 class ContactMailer < ApplicationMailer
-  default to: 'carrick.paul.jeffrey@gmail.com'
-  default subject: 'New Contact Form Submission from paul-carrick.com'
-  default from: 'info@paul-carrick.com'
+  default to: "carrick.paul.jeffrey@gmail.com"
+  default subject: "New Contact Form Submission from paul-carrick.com"
+  default from: "info@paul-carrick.com"
 
   def contact_email(name, email, phone, message)
     @sender_email = email
     @sender_name = name
     @sender_phone = phone
     @sender_message = message
-    mail(to: 'carrick.paul.jeffrey@gmail.com',
-         from: 'info@paul-carrick.com',
-         subject: "Contact Request")
+    mail(to: "carrick.paul.jeffrey@gmail.com",
+         from: "info@paul-carrick.com",
+         subject: "Contact Request") do |format|
+      format.html # Ensure it looks for `contact_email.html.erb`
+    end
   end
 end
