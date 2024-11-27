271f0ba7cfa00ffc800e69a73c198488e0f22ba7
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-11-27 10:13:28 -0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
   before_action :setup_footer_items
   before_action :setup_user
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   private
 
   def setup_main_menu_items
