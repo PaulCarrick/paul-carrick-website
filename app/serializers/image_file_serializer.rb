4115537978d805436a47896a1a384b72134f0e8c
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-12-02 15:21:34 -0800

    Update image_file_serializer.rb

--- app/serializers/image_file_serializer.rb
+++ app/serializers/image_file_serializer.rb
@@ -1,9 +1,16 @@
-class ImageFileSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
+class ImageFileSerializer
+  def initialize(image_file)
+    @image_file = image_file
+  end
 
-  attributes :id, :name, :mime_type, :caption, :description, :image_url
-
-  def image_url
-    rails_blob_url(object.image, only_path: true) if object.image.attached?
+  def as_json(_options = {})
+    {
+      id: @image_file.id,
+      name: @image_file.name,
+      mime_type: @image_file.mime_type,
+      caption: @image_file.caption,
+      description: @image_file.description,
+      image_url: @image_file.image.attached? ? Rails.application.routes.url_helpers.rails_blob_url(@image_file.image, only_path: true) : nil
+    }
   end
 end
