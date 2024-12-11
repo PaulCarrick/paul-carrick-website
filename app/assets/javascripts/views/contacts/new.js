cba6454f14894790ebdb80952fd0af8aa8ed5313
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-12-11 12:33:53 -0800

    Still having touble with CAPTCHA. Rolling back.

---
+++ app/assets/javascripts/views/contacts/new.js
@@ -0,0 +1,10 @@
+// app/assets/javascripts/views/contacts/new.js
+function submitForm(formId, token) {
+debugger;
+  document.getElementById(formId).submit();
+}
+
+function recaptchaError(formId) {
+debugger;
+  document.getElementById(formId).submit();
+}
