a4c3700486551bd80d6b7c32218b86b482781d2b
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-12-11 12:34:22 -0800

    Update new.js

--- app/assets/javascripts/views/contacts/new.js
+++ app/assets/javascripts/views/contacts/new.js
@@ -1,10 +1,8 @@
 // app/assets/javascripts/views/contacts/new.js
 function submitForm(formId, token) {
-debugger;
   document.getElementById(formId).submit();
 }
 
 function recaptchaError(formId) {
-debugger;
   document.getElementById(formId).submit();
 }
