b07dc3d29a7b5448baa48a1d61b32ee0b5c6a0e7
Paul Carrick <paulandvirginiacarrick@gmail.com>
2025-01-04 12:38:30 -0800

    Refactor Image Processing.

--- app/javascript/components/RenderSection.jsx
+++ app/javascript/components/RenderSection.jsx
@@ -6,6 +6,13 @@
 import React from "react";
 import DisplayContent from "./DisplayContent";
 import {dupObject, isTextOnly, isPresent} from "./getDefaultOptions";
+import {
+  handleImageGroup,
+  handleImageArray,
+  processVideoImages,
+  imageFileFindByName,
+  missingImageUrl,
+} from "./imageProcessingUtiliries"
 
 const RenderSection = ({
                          section = null,
@@ -105,19 +112,6 @@ function processSection(section) {
   }
 }
 
-function handleImageGroup(group, formatting) {
-  const imageFiles = imageFilesFindByGroup(group)
-
-  if (imageFiles && (imageFiles.length > 0)) {
-    const newFormatting = addImagesToFormatting(formatting, imageFiles);
-
-    return [null, newFormatting];
-  }
-  else {
-    return [missingImageUrl(), formatting];
-  }
-}
-
 function handleSingleImageFile(section, name) {
   const imageFile = imageFileFindByName(name);
   const results   = imageFile.image_url
@@ -158,38 +152,6 @@ function buildSubsection(section, imageFile, formatting) {
   return [subsection, formatting];
 }
 
-function handleImageArray(imageList, formatting) {
-  const images = imageList.split(",").map(imageName => imageFileFindByName(imageName.trim()));
-
-  return [null, addImagesToFormatting(formatting, images)];
-}
-
-function processVideoImages(contents) {
-  contents.forEach(content => {
-    if (!isPresent(content?.description)) return;
-
-    const match = content.description.match(/VideoImage:\s*"(.+)"/);
-
-    if (match) replaceVideoImageTag(content, match[1]);
-  });
-}
-
-function replaceVideoImageTag(content, name) {
-  const imageFile = imageFileFindByName(name)
-  const imageUrl  = imageFile.image_url;
-
-  if (imageUrl) {
-    let label = imageFile.caption;
-
-    if (!label)
-      label = imageFile.name;
-
-    const videoTag = `<a href="#" onclick="showVideoPlayer('${imageUrl}')">${label}</a>`;
-
-    content.description = content.description.replace(/VideoImage:\s*"(.+)"/, videoTag);
-  }
-}
-
 function swapClasses(formatting) {
   if (formatting.text_classes && formatting.image_classes) {
     formatting.image_classes = formatting.image_classes.replace(/w-\d{2,3}/g, "");
@@ -223,70 +185,6 @@ function flipFormattingSide(formatting) {
   }
 
   return newFormatting;
-}
-
-function addImagesToFormatting(formatting, images) {
-  formatting.slide_show_images = images.map(image => ({
-    image_url:   image.image_url,
-    caption:     image.caption,
-    description: image.description
-  }));
-
-  return formatting;
-}
-
-function imageFilesFindByGroup(group) {
-  const imageData = getImageFiles(`q[group_eq]=${group}`);
-
-  if (imageData && (imageData.length > 0))
-    return imageData;
-  else
-    return missingImageFile();
-}
-
-function imageFileFindByName(name) {
-  const imageData = getImageFiles(`q[name_eq]=${name}`);
-
-  if (imageData && (imageData.length > 0))
-    return imageData[0];
-  else
-    return missingImageFile();
-}
-
-function missingImageFile(name = "Unknown") {
-  const imageFile = {
-    name:        "Missing File",
-    caption:     "Missing File",
-    description: `We could not find an image named: ${name}.`,
-    mime_type:   "image/jpeg",
-    group:       null,
-    image_url:   missingImageUrl(),
-  }
-
-  return imageFile;
-}
-
-function missingImageUrl() {
-  const rootPath = `${location.protocol}//${location.host}`;
-
-  return `${rootPath}/images/missing-image.jpg`;
-}
-
-function getImageFiles(query) {
-  try {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open("GET", `/api/v1/image_files?${query}`, false);
-    xhr.send();
-
-    if (xhr.status === 200)
-      return (JSON.parse(xhr.responseText));
-    else
-      return [missingImageFile("Unknown")];
-  }
-  catch (err) {
-    return [missingImageFile("Unknown")];
-  }
 }
 
 import PropTypes from 'prop-types';
---
+++ app/javascript/components/imageProcessingUtiliries.jsx
@@ -0,0 +1,112 @@
+// /app/javascript/components/image{processingUtilities.jsx
+
+import {isPresent} from "./getDefaultOptions.jsx";
+
+export function handleImageGroup(group, formatting) {
+  const imageFiles = imageFilesFindByGroup(group)
+
+  if (imageFiles && (imageFiles.length > 0)) {
+    const newFormatting = addImagesToFormatting(formatting, imageFiles);
+
+    return [null, newFormatting];
+  }
+  else {
+    return [missingImageUrl(), formatting];
+  }
+}
+
+export function handleImageArray(imageList, formatting) {
+  const images = imageList.split(",").map(imageName => imageFileFindByName(imageName.trim()));
+
+  return [null, addImagesToFormatting(formatting, images)];
+}
+
+export function processVideoImages(contents) {
+  contents.forEach(content => {
+    if (!isPresent(content?.description)) return;
+
+    const match = content.description.match(/VideoImage:\s*"(.+)"/);
+
+    if (match) replaceVideoImageTag(content, match[1]);
+  });
+}
+
+function replaceVideoImageTag(content, name) {
+  const imageFile = imageFileFindByName(name)
+  const imageUrl  = imageFile.image_url;
+
+  if (imageUrl) {
+    let label = imageFile.caption;
+
+    if (!label)
+      label = imageFile.name;
+
+    const videoTag = `<a href="#" onclick="showVideoPlayer('${imageUrl}')">${label}</a>`;
+
+    content.description = content.description.replace(/VideoImage:\s*"(.+)"/, videoTag);
+  }
+}
+
+export function addImagesToFormatting(formatting, images) {
+  formatting.slide_show_images = images.map(image => ({
+    image_url:   image.image_url,
+    caption:     image.caption,
+    description: image.description
+  }));
+
+  return formatting;
+}
+
+export function imageFilesFindByGroup(group) {
+  const imageData = getImageFiles(`q[group_eq]=${group}`);
+
+  if (imageData && (imageData.length > 0))
+    return imageData;
+  else
+    return missingImageFile();
+}
+
+export function imageFileFindByName(name) {
+  const imageData = getImageFiles(`q[name_eq]=${name}`);
+
+  if (imageData && (imageData.length > 0))
+    return imageData[0];
+  else
+    return missingImageFile();
+}
+
+export function missingImageFile(name = "Unknown") {
+  const imageFile = {
+    name:        "Missing File",
+    caption:     "Missing File",
+    description: `We could not find an image named: ${name}.`,
+    mime_type:   "image/jpeg",
+    group:       null,
+    image_url:   missingImageUrl(),
+  }
+
+  return imageFile;
+}
+
+export function missingImageUrl() {
+  const rootPath = `${location.protocol}//${location.host}`;
+
+  return `${rootPath}/images/missing-image.jpg`;
+}
+
+export function getImageFiles(query) {
+  try {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `/api/v1/image_files?${query}`, false);
+    xhr.send();
+
+    if (xhr.status === 200)
+      return (JSON.parse(xhr.responseText));
+    else
+      return [missingImageFile("Unknown")];
+  }
+  catch (err) {
+    return [missingImageFile("Unknown")];
+  }
+}
