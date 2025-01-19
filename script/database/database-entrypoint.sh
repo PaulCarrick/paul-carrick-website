326f42a4aef9a8d7b359fd792c922a0adcf999eb
Paul Carrick <paulandvirginiacarrick@gmail.com>
2025-01-19 10:14:39 -0800

    Update database-entrypoint.sh

--- script/database/database-entrypoint.sh
+++ script/database/database-entrypoint.sh
@@ -7,17 +7,8 @@ set +a
 
 if [ -n "$SSH_PORT" ]; then sudo service ssh start; fi
 
-clusters=""
-main=""
-clusters=`/usr/bin/pg_lsclusters`
-main=`echo ${clusters} | grep -q "main"`
-
-if [ -n main ]; then
-  service postgresql start
-else
-  pg_createcluster 15 main
-  service postgresql start
-fi
+pg_createcluster 15 main
+service postgresql start
 
 su - postgres -c "psql -h ${DB_HOST} -U ${POSTGRES_USER} -c \"ALTER ROLE postgres WITH PASSWORD '${POSTGRES_PASSWORD}';\""
 python /postgres/script/setup-database.py -R false -i true -v 4
